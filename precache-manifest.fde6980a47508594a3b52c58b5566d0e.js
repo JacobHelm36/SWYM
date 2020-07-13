63087b50f18bb62ae92fc487252bee201c2a926e
JacobHelm36 <jakehelm36@gmail.com>
2020-07-13 15:54:34 -0600

    Updates

---
+++ precache-manifest.fde6980a47508594a3b52c58b5566d0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f172c1b134be8c662df5260b8352999",
+    "url": "/SWYM/index.html"
+  },
+  {
+    "revision": "701d2641e10f35d0d029",
+    "url": "/SWYM/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d6e51bb71adc32dd6544",
+    "url": "/SWYM/static/css/main.acb0a14b.chunk.css"
+  },
+  {
+    "revision": "701d2641e10f35d0d029",
+    "url": "/SWYM/static/js/2.b35341dc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/SWYM/static/js/2.b35341dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e51bb71adc32dd6544",
+    "url": "/SWYM/static/js/main.5dd5dee8.chunk.js"
+  },
+  {
+    "revision": "15da8d1d57edc90f569c",
+    "url": "/SWYM/static/js/runtime-main.e91f01e7.js"
+  }
+]);
